0de2daeef0ce8b2404bb5cb557db1d0ca974fda0
William Jackson <william@jaackson.com>
2025-03-13 16:04:38 +1000

    puppeteer setup

---
+++ puppeteer/src/index.ts
@@ -0,0 +1,12 @@
+import puppeteer from "puppeteer";
+
+
+async function main() {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com');
+    await page.screenshot({ path: 'screenshot.png' });
+    await browser.close();
+}
+
+main();
