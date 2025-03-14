4c2ff1e56e2db61965706efdfd169fb28ecbca8d
William Jackson <william@jaackson.com>
2025-03-14 21:13:46 +1000

    refactor caching behaviours.

--- puppeteer/src/campusgroups/auth.ts
+++ puppeteer/src/campusgroups/auth.ts
@@ -3,6 +3,17 @@ import puppeteer, { Cookie, Page } from "puppeteer";
 import { TOTP } from "totp-generator";
 import config from "../../config.json";
 
+async function waitForPage(page: Page, expectedUrl: string, navigationCount: number = 1) {
+    for (let i = 0; i < navigationCount; i++) {
+        await page.waitForNavigation().catch((e) => {
+            console.log(`Failed to navigate to ${expectedUrl}, ${i} of ${navigationCount}, ${page.url()}, ${e}`);
+        });
+    }
+    if (page.url() !== expectedUrl) {
+        throw new Error(`Failed to navigate to ${expectedUrl}`);
+    }
+}
+
 // get a new copy of authentication cookies.
 async function newAuthCookies(
     student_id: string,
@@ -15,26 +26,21 @@ async function newAuthCookies(
 
     // Navigate to Campus Groups login page
     await page.goto('https://www.campusgroups.com/shibboleth/login?idp=griffith&school=griffith')
+    await page.waitForSelector('#username');
     await page.type('#username', student_id);
     await page.type('#password', password);
     await page.keyboard.press('Enter');
 
-    // wait for the page to navigate three times to get to the authenticator page, and verify it made it.
-    await page.waitForNavigation();
-    await page.waitForNavigation();
-    await page.waitForNavigation();
-    if (page.url() !== 'https://authenticator.pingone.com.au/pingid/ppm/auth')
-        throw new Error('Failed to navigate to authenticator page.');
+    await waitForPage(page, 'https://authenticator.pingone.com.au/pingid/ppm/auth', 3);
     
     // move to the devices page.
     await page.goto('https://authenticator.pingone.com.au/pingid/ppm/devices');
+    await page.waitForSelector(`[data-id="${otp_id}"]`);
     await page.click(`[data-id="${otp_id}"]`);
     await page.click('#device-submit')
     
-    await page.waitForNavigation();
-    await page.waitForNavigation();
-    if (page.url() !== 'https://authenticator.pingone.com.au/pingid/ppm/auth')
-        throw new Error('Failed to navigate back to authenticator page.');
+    await waitForPage(page, 'https://authenticator.pingone.com.au/pingid/ppm/auth', 2);
+    await page.waitForSelector('#otp');
 
     // // Enter PingID OTP
     const { otp } = TOTP.generate(otp_secret, { digits: 6 });
@@ -42,12 +48,7 @@ async function newAuthCookies(
     await page.type('#otp', otp);
     await page.click('input[type="submit"]')
     
-    await page.waitForNavigation();
-    await page.waitForNavigation();
-    await page.waitForNavigation();
-    await page.waitForNavigation();
-    if (page.url() !== 'https://griffith.campusgroups.com/groups')
-        throw new Error('Failed to navigate to Campus Groups page.');
+    await waitForPage(page, 'https://griffith.campusgroups.com/groups', 4);
     
     // Get cookies
     const browser_cookies = await browser.cookies();
@@ -56,28 +57,41 @@ async function newAuthCookies(
     return browser_cookies.filter(cookie => cookie.domain == 'griffith.campusgroups.com')
 } 
 
-// get a cached/new copy of authentication cookies.
-export async function getAuthCookies(): Promise<Cookie[] | null> {
-    const cached_cookies = await redisClient.get('cache:auth-cookies');
-    if (cached_cookies) {
-        return JSON.parse(cached_cookies);
-    }
+async function updateAuthCookies(): Promise<Cookie[]> {
+    // set the cache to alive.
+    // if another process requests the cache while this one is updating, it will return the OLD cached cookies.
+    // once this process is finished, it will finish updating the cache.
+    await redisClient.setEx('cache:auth-cookies:alive', config.cookieExpiry, '1');
 
     const cookies = await newAuthCookies(
         process.env.STUDENT_ID!,
         process.env.PASSWORD!,
         process.env.OTP_ID!,
         process.env.OTP_SECRET!,
-    ).catch((e) => {
-        console.error(e);
-        return null;
-    });
+    );
 
-    if (cookies) {
-        await redisClient.setEx('cache:auth-cookies', config.cookieExpiry, JSON.stringify(cookies));
+    await redisClient.set('cache:auth-cookies', JSON.stringify(cookies));
+    return cookies;
+}
+
+// get a cached/new copy of authentication cookies.
+export async function getAuthCookies(): Promise<Cookie[] | null> {
+    let [cached_cookies, alive] = await redisClient.multi()
+        .get('cache:auth-cookies')
+        .get('cache:auth-cookies:alive')
+        .exec();
+
+    // if no cache exists, update and return the updated cookies.
+    if (!cached_cookies) {
+        return await updateAuthCookies();
     }
 
-    return cookies;
+    // if the cache is expired, update in the background and return the cached cookies.
+    if (!alive) {
+        updateAuthCookies();
+    }
+
+    return JSON.parse(cached_cookies.toString());
 }
 
 export async function setAuthCookies(page: Page) {
