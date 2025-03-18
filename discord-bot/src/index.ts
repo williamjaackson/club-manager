d9adb5346c890a9fd8711f6d4a76efcece404c75
William Jackson <william@jaackson.com>
2025-03-18 14:33:09 +1000

    connect to redis from discord-bot

--- discord-bot/src/index.ts
+++ discord-bot/src/index.ts
@@ -1,4 +1,5 @@
 import { Client, GatewayIntentBits } from "discord.js";
+import { redisClient } from "./lib/redis";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -7,4 +8,14 @@ client.once("ready", (client: Client<true>) => {
 });
 
 console.log("Starting bot...");
-client.login(process.env.DISCORD_TOKEN);
+
+client.login(process.env.DISCORD_TOKEN).then(() => {
+  redisClient
+    .connect()
+    .catch((err) => {
+      console.error("Failed to connect to Redis:", err);
+    })
+    .then(() => {
+      console.log("Connected to Redis");
+    });
+});
