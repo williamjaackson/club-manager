5a6caf38121bfeac40159ecfb0024ea71775e1e4
William Jackson <william@jaackson.com>
2025-03-20 17:22:10 +1000

    autoupdate with interval

--- puppeteer/src/index.ts
+++ puppeteer/src/index.ts
@@ -2,8 +2,8 @@ import { redisClient } from "./lib/redis";
 import { newSession, runGriffithAuthFlow } from "./campusgroups/auth";
 import puppeteer from "puppeteer";
 import { updateMemberList } from "./campusgroups/members";
-
-async function main() {
+import config from "../config.json";
+async function updateMemberLists() {
   const browser = await puppeteer.launch();
   await runGriffithAuthFlow(browser);
 
@@ -25,10 +25,26 @@ async function main() {
   ]);
 
   await browser.close();
-
-  console.log("done");
 }
 
+async function main() {
+  let retryCount = 0;
+  async function handler() {
+    try {
+      await updateMemberLists();
+    } catch (err) {
+      console.error("Failed to update member lists:", err);
+      retryCount++;
+      if (retryCount > 3) {
+        throw err;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 1000 * 5));
+      await handler();
+    }
+  }
+
+  await handler();
+}
 redisClient
   .connect()
   .catch((err) => {
@@ -37,4 +53,5 @@ redisClient
   .then(() => {
     console.log("Connected to Redis");
     main();
+    setInterval(main, config.updateInterval * 1000);
   });
