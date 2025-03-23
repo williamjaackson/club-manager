a2e9f99c133695444bc4495d66134ebcdded0634
William Jackson <william@jaackson.com>
2025-03-23 19:56:16 +1000

    merge discordUser and discordMember

--- discord-bot/src/redisEventHandler.ts
+++ discord-bot/src/redisEventHandler.ts
@@ -25,22 +25,6 @@ export async function redisEventHandler(client: Client) {
       return;
     }
 
-    const discordUser = await client.users.fetch(
-      discordUserRecord.discord_user_id
-    );
-
-    if (discordUser.id !== "817515772317925407") {
-      console.log("TESTING, WILL NOT DM");
-      return;
-    }
-
-    await discordUser.send(`**Thank you for joining the Griffith ICT Club (${clubArea}) on CampusGroups!**
-You've been given club-member access on the discord, and have now been connected as:
-\`\`\`
-${campusUserRecord.first_name} ${campusUserRecord.last_name}
-\`\`\`
--# This will only ever be viewable by verified students, or staff members.`);
-
     const guild = await client.guilds.fetch(config.guildId);
     const role = await guild.roles.fetch(config.roleId);
 
@@ -49,7 +33,26 @@ ${campusUserRecord.first_name} ${campusUserRecord.last_name}
       return;
     }
 
-    const discordMember = await guild.members.fetch(discordUser.id);
+    const discordMember = await guild.members.fetch(
+      discordUserRecord.discord_user_id
+    );
+
+    if (!discordMember) {
+      console.log("Discord member not found");
+      return;
+    }
+
+    if (discordMember.id !== "817515772317925407") {
+      console.log("TESTING, WILL NOT DM");
+      return;
+    }
+
+    await discordMember.send(`**Thank you for joining the Griffith ICT Club (${clubArea}) on CampusGroups!**
+You've been given club-member access on the discord, and have now been connected as:
+\`\`\`
+${campusUserRecord.first_name} ${campusUserRecord.last_name}
+\`\`\`
+-# This will only ever be viewable by verified students, or staff members.`);
 
     await discordMember.roles.add(role);
 
