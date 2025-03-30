1ba55eb9453b91cb156fa890aa7f662f4a530350
William Jackson <wcjaackson@gmail.com>
2025-03-31 07:53:54 +1000

    remove useless comments

--- discord-bot/src/commands/sync-roles.ts
+++ discord-bot/src/commands/sync-roles.ts
@@ -57,16 +57,8 @@ export async function execute(interaction: ChatInputCommandInteraction) {
   }
 
   const results = new Map<string, string>();
-  // for testing, only use this discord member: 817515772317925407, 874415216186757181
-  // const testDiscordMembers = ["817515772317925407", "874415216186757181"];
-  // const testDiscordMembersMap = new Map(
-  // testDiscordMembers.map((id) => [id, discordMembersMap.get(id)])
-  // );
   for (const [discordId, discordMember] of discordMembersMap.entries()) {
-    // if (!testDiscordMembers.includes(discordId)) continue;
-
     const campusMember = campusMembersMap.get(discordId);
-    // if () continue;
 
     const result = await syncRoles(discordMember, campusMember);
     if (result) results.set(discordId, result);
@@ -83,47 +75,4 @@ export async function execute(interaction: ChatInputCommandInteraction) {
       ),
     ],
   });
-
-  // const channel: TextChannel = interaction.options.getChannel("channel", true);
-
-  // const btn_link = new ButtonBuilder()
-  //   .setCustomId("flow:connect:0")
-  //   .setLabel("Connect sNumber")
-  //   .setEmoji(config.emoji)
-  //   .setStyle(ButtonStyle.Secondary);
-
-  // const btn_exempt = new ButtonBuilder()
-  //   .setCustomId("flow:exempt:0")
-  //   .setLabel("Request Exemption")
-  //   .setStyle(ButtonStyle.Danger);
-
-  // const row = new ActionRowBuilder().addComponents(btn_link, btn_exempt);
-
-  // const embed = new EmbedBuilder()
-  //   .setTitle("Connect your Griffith sNumber")
-  //   .setDescription(
-  //     "To access the rest of this server,\nYou need to connect your __Griffith sNumber__.\nClick the button below to get started."
-  //   )
-  //   .addFields({
-  //     name: "Don't Have an sNumber?",
-  //     value:
-  //       "You can apply for an exemption if:\n" +
-  //       "• You are a student of a different university,\n" +
-  //       "• You are a past student of Griffith University,\n" +
-  //       "• You need access to this server for another reason.",
-  //   })
-  //   .setThumbnail(interaction.client.user?.displayAvatarURL())
-  //   .setColor(0x2b2d31)
-  //   .setFooter({
-  //     text: "Griffith Connect • Powered by Griffith ICT Club <https://www.griffithict.com/>",
-  //   });
-
-  // await channel.send({
-  //   embeds: [embed],
-  //   components: [row as ActionRowBuilder<ButtonBuilder>],
-  // });
-
-  // await interaction.editReply({
-  //   content: "Setup complete!",
-  // });
 }
